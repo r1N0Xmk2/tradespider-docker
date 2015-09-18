0abe869bd4d4cb556466564f1d5ab28a73e1d9ca
r1N0Xmk2 <rene7297152@gmail.com>
2015-09-18 11:43:21 +0800

    modify deploy

--- deploy-spider.sh
+++ deploy-spider.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 cd ../tradespider 
+echo "$(pwd)in tradespider"
 git pull origin docker
 echo "update tradespider"
 cd ../glassesbag
 git pull central pg
+cd CaptchaServer
 npm i
 echo "update glassesbag"
 
 echo "start tradespider"
-cd ../tradespider-docer
+cd ../../tradespider-docker
 docker-compose stop && \
 echo y | docker-compose rm && \
 docker-compose up -d && \
